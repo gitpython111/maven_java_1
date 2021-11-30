5072f20441ae4de985096c030a6a2eb4ec4c14bf
wjh <wjh@qq.com>
2021-11-30 22:54:21 +0800

    first commit

---
+++ src/main/java/cn/itcast/demo/HelloWorld.java
@@ -0,0 +1,7 @@
+package cn.itcast.demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven ~~~");
+    }
+}
---
+++ src/test/java/cn/itcast/test/TestHello.java
@@ -0,0 +1,11 @@
+package cn.itcast.test;
+
+import org.junit.Test;
+
+public class TestHello {
+    @Test
+    public void test1(){
+        System.out.println("=======>>>>>>>>>Hello test maven");
+    }
+
+}
