782ef8d2c045c308d137281370626abfa7e91f8a
wjh <wjh@qq.com>
2021-11-30 23:29:04 +0800

    third commit

--- src/main/java/cn/itcast/demo/HelloWorld.java
+++ src/main/java/cn/itcast/demo/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Maven ~~~");
         System.out.println("Hello Maven2 ~~~");
+        System.out.println("Hello Maven3 ~~~");
     }
 }
